0e6ae67ad13f32de4e0f1a89a770289db8848402
Simon McKenna <simon.mckenna@hotmail.co.uk>
2026-02-07 11:52:13 +0000

    Log rotate setup for the jukebox app and uwsgi

--- app/config.py
+++ app/config.py
@@ -6,7 +6,6 @@
 import logging
 import os
 
-
 ################################################################
 # Logging configuration
 ################################################################
@@ -17,9 +16,8 @@ import os
 # logging.INFO      20
 # logging.WARNING   30
 # logging.ERROR     40
-JBOX_PROD_LOGFILE = "/var/log/jbox/jbox.log"
+JBOX_PROD_LOGFILE = "/srv/jukebox/log/jbox.log"
 JBOX_PROD_LOGLEVEL = logging.INFO
-JBOX_PROD_LOGMODE = "PROD"
 # How many Days to retain the logfile for 
 LOG_FILE_RETENTION = 7   # Updatable in postgres Parameters
 
@@ -27,7 +25,7 @@ JBOX_DEV_LOGFILE = "logfile/bkstgsvc.log"
 JBOX_DEV_LOGLEVEL  = logging.DEBUG
 
 JBOX_LOGLEVEL      = JBOX_DEV_LOGLEVEL # Updatable in postgres Parameter
-JBOX_LOGFILE       = JBOX_DEV_LOGFILE
+JBOX_LOGFILE       = JBOX_PROD_LOGFILE
 
 # Jukebox Listener spotify expects to see either 7777 or 8888
 JBOX_LISTENER_PORT = 8888
@@ -49,11 +47,8 @@ ALLOWED_IP="127.0.0.0/24"
 JBOX_TOKEN = "DUMMY-VALUE"
 JBOX_TOKEN_NAME = "X-JBOX-AUTH-TOKEN"
 
-# Delay for the background scheduler
-JBOX_SCHEDULER_DELAY = 2
-
 # Global variable to hold the database filename
-SQLDBNAME= "/home/jbox/jukebox.db"
+SQLDBNAME= "/srv/jukebox/db/jukebox.db"
 # Global variable to hold the homepage filename
 JBOX_HOME = 'static/jukebox.html'
 
